077d11a7fe6856318a523abf155f21d36dde3a15
Konstantin Gorshkov <const.potter@gmail.com>
2020-01-02 15:32:34 +0600

    Added option `format`

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 'use strict';
 
+const defaultFormat = {
+  boolean: value => (value ? '1' : '0'),
+  number: value => value.toString(),
+  date: value => value.getTime(),
+  object: value => JSON.stringify(value)
+};
+
 class Stringifier {
-  constructor({ delimiter = ',' }) {
+  constructor({ delimiter = ',', format = {} }) {
     this.delimiter = delimiter;
+    this.format = { ...defaultFormat, ...format };
   }
   read(data) {
-    return data.reduce((acc, cur) => acc + this._readRow(cur), '');
+    return data.reduce((acc, cur, i) => acc + this._readRow(cur, i), '');
   }
-  _readRow(row) {
+  _readRow(row, index) {
     if (Array.isArray(row)) {
-      const str = row.join(this.delimiter);
+      const str = row
+        .map((value, i) => this._format(value, { column: i, row: index }))
+        .join(this.delimiter);
       return str + '\n';
     }
+  }
+  _format(value, context) {
+    let type = typeof value;
+    if (value instanceof Date) {
+      type = 'date';
+    }
+    const handler = this.format[type];
+    if (handler && !(typeof handler === 'function')) {
+      throw new Error(`format[${type}] must be a function`);
+    }
+    return handler ? handler(value, context) : value;
   }
 }
 
@@ -24,8 +45,12 @@ const stringify = (records, options, cb) => {
     return cb(new Error('Invalid records: records must be array'));
   }
   const stringifier = new Stringifier(options);
-  const str = stringifier.read(records);
-  cb(null, str);
+  try {
+    const str = stringifier.read(records);
+    cb(null, str);
+  } catch (e) {
+    cb(e);
+  }
 };
 
 module.exports = stringify;
---
+++ test/options.format.test.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const assert = require('assert'),
+  stringify = require('../index');
+
+describe('Option `format`', () => {
+  it('with default value', () => {
+    const date = new Date();
+    stringify(
+      [['string', 1, { a: 1, b: '2', c: true }, date, true, false]],
+      (err, data) => {
+        assert.equal(err, null);
+        assert.equal(data,
+          `string,1,{"a":1,"b":"2","c":true},${date.getTime()},1,0\n`);
+      }
+    );
+  });
+  it('with custom value', () => {
+    const date = new Date();
+    stringify(
+      [['string', 3, { a: 1, b: '2', c: true }, date, true, false]],
+      {
+        format: {
+          string: value => value.toUpperCase(),
+          number: (value, context) => (value * (context.column + 1)).toString(),
+          boolean: value => value,
+          date: value => value.getTime() + 100,
+          object: value => JSON.stringify(Object.entries(value))
+        }
+      },
+      (err, data) => {
+        const dateStr = date.getTime() + 100;
+        assert.equal(err, null);
+        assert.equal(data,
+          `STRING,6,[["a",1],["b","2"],["c",true]],${dateStr},true,false\n`
+        );
+      }
+    );
+  });
+  it('return error when formatter is not a function', () => {
+    stringify(
+      [['a', 'b'], [1, 3]],
+      { format: { number: 10 } },
+      err => {
+        assert.equal(err.message, 'format[number] must be a function');
+      }
+    );
+  });
+});
